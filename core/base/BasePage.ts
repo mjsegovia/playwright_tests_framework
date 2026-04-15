87f8082e3018d3e2c35368a235f9c70b70f8ab6e
mjsegovia <mj.segovia22@gmail.com>
2026-04-15 15:17:06 -0400

    add container id from the main page

--- core/base/BasePage.ts
+++ core/base/BasePage.ts
@@ -1,14 +1,28 @@
-import { Page } from '@playwright/test';
+import { Page, Locator } from '@playwright/test';
+import { Logger } from '../logger';
 
 export abstract class BasePage {
   protected page: Page;
+  protected contenedorPrincipal: Locator;
 
-  constructor(page: Page) {
+  constructor(page: Page) {     
     this.page = page;
+     this.contenedorPrincipal = page.locator('#page-content');    
   }
 
-  async navigateTo(url: string ='/') {
+  async goto(url: string ='/') {
+    Logger.action(`Navigating to ${url}`);
     await this.page.goto(url);
+  }
+
+  async click(locator:Locator, name: string) {
+    Logger.action(`Clicking on ${name}`);
+    await locator.click();
+  }
+
+  async fill(locator:Locator, value: string, name: string) {
+    Logger.action(`Filling ${name} with ${value}`);
+    await locator.fill(value);
   }
 
 // async goto(): Promise<void> {
