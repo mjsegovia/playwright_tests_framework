e60ad3554d6808990b77b74115850698e43a7c6f
mjsegovia <mj.segovia22@gmail.com>
2026-04-18 12:33:06 -0400

    change the base url of the project

--- config/config.ts
+++ config/config.ts
@@ -1,15 +1,13 @@
 export const urls = {
   // Application URLs
   playwright: 'https://playwright.dev/',
-  myshopify: 'https://sauce-demo.myshopify.com/',  
+  bstackdemo: 'https://bstackdemo.com/',  
 };
 
 export const config = {
-  base_url: process.env.BASE_URL || 'https://sauce-demo.myshopify.com/',
+  base_url: process.env.BASE_URL || 'https://bstackdemo.com/',
   api_url: process.env.API_URL || 'https://reqres.in/api',
   env: process.env.ENV || 'development',
   headless: process.env.HEADLESS !== 'false',
-  slowMo: process.env.SLOWMO ? parseInt(process.env.SLOWMO) : 0,
-
- 
+  slowMo: process.env.SLOWMO ? parseInt(process.env.SLOWMO) : 0, 
 };
