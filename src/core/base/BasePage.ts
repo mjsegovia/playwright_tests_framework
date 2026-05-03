b5d87da2bcd3da91ef7af0effafd8ab9840ee820
mjsegovia <mj.segovia22@gmail.com>
2026-05-03 16:23:55 -0400

    refactor base page  class

--- src/core/base/BasePage.ts
+++ src/core/base/BasePage.ts
@@ -5,33 +5,30 @@ import { Logger } from '../logger';
 export abstract class BasePage {
   protected page: Page;
 
-  constructor(page: Page) {     
-    this.page = page;        
+  constructor(page: Page) {
+    this.page = page;
   }
 
   async getUrl(): Promise<string> {
-  return this.page.url();
-}
+    return this.page.url();
+  }
 
-  async goto(url: string ='/') {
+  async navigate(url: string = '/') {
     Logger.action(`Navigating to ${url}`);
     await this.page.goto(url);
   }
 
-  async click(locator:Locator, name: string) {
-  Logger.action(`Clicking on ${name}`);
-  await locator.click();
+  async click(locator: Locator, name: string) {
+    Logger.action(`Clicking on ${name}`);
+    await locator.click();
   }
 
-  async fill(locator:Locator, value: string, name: string) {
+  async fill(locator: Locator, value: string, name: string) {
     Logger.action(`Filling ${name} with ${value}`);
     await locator.fill(value);
   }
 
- //async goto(): Promise<void> {
-  //await this.page.goto(this.url);
-
   async waitForPageLoad(): Promise<void> {
-    await this.page.waitForLoadState('networkidle');
+    await this.page.waitForLoadState('domcontentloaded');
   }
 }
