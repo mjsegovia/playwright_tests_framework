f4e5aa114b6919dfa1649f03b1478205fdeb60f4
mjsegovia <mj.segovia22@gmail.com>
2026-05-03 20:23:56 -0400

    modify local  and CI report, and change the projects

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,42 +1,59 @@
 import { defineConfig, devices } from '@playwright/test';
-import {config} from './src/config/config';
+import { config } from './src/config/config';
 
 export default defineConfig({
-  testDir: './tests',    /* Run tests in files in parallel */
-  fullyParallel: true,   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  
+  testDir: './tests' /* Run tests in files in parallel */,
+  fullyParallel: true /* Fail the build on CI if you accidentally left test.only in the source code. */,
+  forbidOnly: !!process.env.CI /* Retry on CI only */,
+  retries: process.env.CI ? 2 : 0 /* Opt out of parallel tests on CI. */,
+  workers: process.env.CI
+    ? 2
+    : undefined /* Reporter to use. See https://playwright.dev/docs/test-reporters */,
+  reporter: process.env.CI
+    ? [['github'], ['html', { open: 'never' }]]
+    : [
+        ['list', { printSteps: true }],
+        ['html', { open: 'on-failure' }],
+      ],
+
   use: {
-    baseURL: config.base_url,   
-    trace: 'on-first-retry',
-    extraHTTPHeaders: {
-      'x-api-key': process.env.REQRES_API_KEY || '',
-    },
+    baseURL: config.base_url,
+    trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    // Uncomment this only if the API requires an 'x-api-key' header for every request
+    // extraHTTPHeaders: {
+    //    'x-api-key': process.env.REQRES_API_KEY || '',
+    //  },
   },
 
   projects: [
     {
-      name: 'setup',
+      name: 'auth-setup',
       testMatch: /auth\.setup\.ts/,
     },
     {
-      name: 'chromium',
-      use: { 
+      name: 'authenticated-chromium',
+      use: {
         ...devices['Desktop Chrome'],
-      storageState: 'tests/.auth/user.json'
+        storageState: 'tests/.auth/user.json',
+      },
+      dependencies: ['setup'],
     },
-    dependencies: ['setup'],
-  },
+    {
+      name: 'chromium-logged-out',
+      testMatch: /ui\/public\/.*\.spec\.ts/,
+      use: {
+        ...devices['Desktop Chrome'],
+        storageState: undefined,
+      },
+    },
 
     /* Test against branded browsers. */
     {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' },
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
     },
-
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
@@ -62,5 +79,4 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
 });
